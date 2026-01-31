9ee446c18058db2f8ec34194e915f4df5aeb5ed0
Liam5650 <62405326+Liam5650@users.noreply.github.com>
2026-02-01 00:30:36 +0900

    Update explanation

--- Arrays-&-Hashing/Remove-Element.py
+++ Arrays-&-Hashing/Remove-Element.py
@@ -9,6 +9,13 @@ class Solution:
         # we simply iterate through, swapping valid nums to the pointer
         # and incrementing, and skipping invalid. 
 
+        # The reason this works is that we know for sure that all values
+        # before the one we have reached have already been processed and 
+        # moved within the valid k range, so we dont have to worry about 
+        # overwriting anything outside of it (i.e., before the current
+        # index but after the k index, as these have been processed
+        # already and can be safetly overwritten).
+
         k = 0
         for i in range(len(nums)):
             if nums[i] != val: 
